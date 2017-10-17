13ead68b5ab24b328db7707907ac131a643db7d8
T-Midnight <belousovatanya1995@gmail.com>
2017-10-18 01:01:14 +0400

    Layout home page

---
+++ index.php
@@ -0,0 +1,36 @@
+<html>
+  <head>
+    <link 
+      rel = "stylesheet" type= "text/css"  href = "main.css"/>
+    <title>Электронный дневник школьника</title>
+  </head>
+  <body>
+    <div class="container-fluid header">
+		<p> Электронный дневник школьника </p>
+    </div>
+	<aside>
+
+			<ul>
+				<li><a href="/donec/">Личные данные</a></li>
+				<li><a href="/donec/">Список группы</a></li>
+				<li><a href="/vestibulum/">Оценки</a></li>
+				<li><a href="/etiam/">Домашнее задание</a></li>
+			</ul>
+	</aside>
+	<div class="container">
+		<div class="photo">
+			<img src="Petr.jpg" alt="Пётр Михайлов">
+		</div>
+		<h2>Михайлов Пётр </h2> <br>
+		<h4>Ученик 5 класса Б многопрофильного лицея № 2456 в г. Москва, Россия </h4>
+	</div>
+	<footer>
+		<div id="footer">
+		<div id="footer-copyrite">
+			<p>Copyright © 2017 Белоусова Татьяна ПИбд-31</p> 
+		</div>
+		</div>
+	</footer>
+  </body>
+</html>
+
